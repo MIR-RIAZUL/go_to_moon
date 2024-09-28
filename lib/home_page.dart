59592a39b035114a99f8c3b4271b4d43612a7f6f
MIR-RIAZUL <mirriazul859@gmail.com>
2024-09-29 02:32:57 +0600

    edit of padding

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,21 +2,48 @@ import 'package:flutter/material.dart';
 
 class Homepage extends StatelessWidget{
 
-  const Homepage({Key?key}):super(key:key);
+ late  double _deviceHeight,_deviceWidth;
+
+   Homepage({Key?key}):super(key:key);
 
   @override
   Widget build(BuildContext context) {
+    
+    _deviceHeight=MediaQuery.of(context).size.height;
+    _deviceWidth=MediaQuery.of(context).size.width;
    return Scaffold(
-     body:Container(
-       decoration: BoxDecoration(
-         color: Colors.red,
-         image:DecorationImage(
-           fit: BoxFit.fitHeight,
-           image: AssetImage("assets/images/go_moon.jpg"),
-         )
-       ),
-     ),
+     body:SafeArea(
+         child:Container(
+           height:_deviceHeight ,
+       width: _deviceWidth,
+       padding: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.05,),
+       child: _PageTitle(),
+      )
+     )
    );
+  }
+
+  Widget _PageTitle()
+  {
+    return const Text(" GO MOON",
+      style:TextStyle(
+          color: Colors.blueGrey,
+          fontSize: 70,
+        fontWeight: FontWeight.w800,
+      ),
+      );
+  }
+
+  Widget _astroImageWidget()
+  {
+    return Container(
+      decoration: BoxDecoration(
+          image:DecorationImage(
+            fit: BoxFit.fitHeight,
+            image: AssetImage("assets/images/go_moon.jpg"),
+          )
+      ),
+    );
   }
 }
 
