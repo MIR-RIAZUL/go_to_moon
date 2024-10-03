7d4ae9ae9f7e91bc6d7ed4f030b810ddd9f8690c
MIR-RIAZUL <mirriazul859@gmail.com>
2024-10-04 04:34:03 +0600

    little bit change 3

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,69 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:go_to_moon/widgets/custom_dropdown_button.dart';
 
-class Homepage extends StatelessWidget{
+class Homepage extends StatelessWidget {
+  late double _deviceHeight, _deviceWidth;
 
- late  double _deviceHeight,_deviceWidth;
-
-   Homepage({Key?key}):super(key:key);
+  Homepage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    
-    _deviceHeight=MediaQuery.of(context).size.height;
-    _deviceWidth=MediaQuery.of(context).size.width;
-   return Scaffold(
-     body:SafeArea(
-         child:Container(
-           height:_deviceHeight ,
-       width: _deviceWidth,
-       padding: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.05,),
-       child:Column(
-         mainAxisAlignment:MainAxisAlignment.spaceBetween,
-         mainAxisSize: MainAxisSize.max,
-         crossAxisAlignment:CrossAxisAlignment.start ,
-         children: [
-           _PageTitle(),
-           _destinationDropDownWidget()
-
-         ],
-       )
-      )
-     )
-   );
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+        body: SafeArea(
+            child: Container(
+                height: _deviceHeight,
+                width: _deviceWidth,
+                padding: EdgeInsets.symmetric(
+                  horizontal: _deviceWidth * 0.05,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [_PageTitle(), _destinationDropDownWidget(),
+                    _travellerInformationWidget()],
+                ))));
   }
 
-  Widget _PageTitle()
-  {
-    return const Text("    GO MOON",
-      style:TextStyle(
-          color: Colors.blueGrey,
-          fontSize: 70,
+  Widget _PageTitle() {
+    return const Text(
+      "    GO MOON",
+      style: TextStyle(
+        color: Colors.blueGrey,
+        fontSize: 70,
         fontWeight: FontWeight.w800,
-      ),
-      );
-  }
-
-  Widget _astroImageWidget()
-  {
-    return Container(
-      decoration: BoxDecoration(
-          image:DecorationImage(
-            fit: BoxFit.fitHeight,
-            image: AssetImage("assets/images/go_moon.jpg"),
-          )
       ),
     );
   }
 
-Widget _destinationDropDownWidget()
-{
-return customDropDownButton(values:const[
-  'james web station',
-  'preneure station',
-], width: _deviceWidth,);
+  Widget _astroImageWidget() {
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+        fit: BoxFit.fitHeight,
+        image: AssetImage("assets/images/go_moon.jpg"),
+      )),
+    );
+  }
+
+  Widget _destinationDropDownWidget(){
+    return customDropDownButton(values:const[
+      'james web station',
+      'preneure station',
+    ], width: _deviceWidth,);
+  }
+  Widget _travellerInformationWidget()
+  {
+    return customDropDownButton(
+      values: const [
+        '1',
+        '2',
+        '3',
+        '4'
+      ],
+      width: _deviceWidth *0.45,
+    );
+  }
 }
-}
-
-
-
