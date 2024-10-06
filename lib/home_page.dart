bed8c7da59f970d7c690bbc2fede45817d79d66f
MIR-RIAZUL <mirriazul859@gmail.com>
2024-10-07 02:02:32 +0600

    final project

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -18,25 +18,22 @@ class Homepage extends StatelessWidget {
                 padding: EdgeInsets.symmetric(
                   horizontal: _deviceWidth * 0.05,
                 ),
-                child:Stack(children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    mainAxisSize: MainAxisSize.max,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [_PageTitle(),
-                      _bookRideWidget()],
+                child: Stack(
+                  children: [
+                    _astroImageWidget(), // Image fills the whole screen
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        _pageTitleWidget(),
+                        _bookRideWidget(),
+                      ],
                     ),
-                 Align(
-                   alignment: Alignment.centerRight,
-                   child:  _astroImageWidget(),)
                   ],
-                )
-            )
-        )
-    );
+                ))));
   }
 
-  Widget _PageTitle() {
+  Widget _pageTitleWidget() {
     return const Text(
       "    GO MOON",
       style: TextStyle(
@@ -49,75 +46,74 @@ class Homepage extends StatelessWidget {
 
   Widget _astroImageWidget() {
     return Container(
-      height: _deviceHeight *0.50,
-      width: _deviceWidth *0.65,
-      decoration: BoxDecoration(
-          image: DecorationImage(
-        fit: BoxFit.fitHeight,
-        image: AssetImage("assets/images/go_moon.jpg"),
-      )),
+      height: double.infinity,
+      width: double.infinity,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage("assets/images/go_moon.jpg"),
+        ),
+      ),
     );
   }
-  Widget _bookRideWidget()
-  {
-return Container(height: _deviceHeight *0.25,
-  child: Column(
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    mainAxisSize: MainAxisSize.max,
-    crossAxisAlignment: CrossAxisAlignment.center,
-    children: [
-      _destinationDropDownWidget(),
-      _travellerInformationWidget(),
-      _ridebatton(),
-    ],
-  ),
-);
+
+  Widget _bookRideWidget() {
+    return Container(
+      height: _deviceHeight * 0.25,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _destinationDropDownWidget(),
+          _travellerInformationWidget(),
+          _rideButton(),
+        ],
+      ),
+    );
   }
 
-  Widget _destinationDropDownWidget(){
-    return customDropDownButton(values:const[
-      'james web station',
-      'preneure station',
-    ], width: _deviceWidth,);
+  Widget _destinationDropDownWidget() {
+    return customDropDownButton(
+      values: const [
+        'James Webb Station',
+        'Preneure Station',
+      ],
+      width: _deviceWidth,
+    );
   }
-  Widget _travellerInformationWidget()
-  {
+
+  Widget _travellerInformationWidget() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      mainAxisSize: MainAxisSize.max,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         customDropDownButton(
-          values: const [
-            '1',
-            '2',
-            '3',
-            '4'
-          ],
-          width: _deviceWidth *0.45,
+          values: const ['1', '2', '3', '4'],
+          width: _deviceWidth * 0.45,
         ),
-
         customDropDownButton(
-          values: const [
-            'economy',
-            'business',
-            'first class',
-            '2nd class'
-          ],
-          width: _deviceWidth *0.40,
+          values: const ['Economy', 'Business', 'First Class', '2nd Class'],
+          width: _deviceWidth * 0.40,
         ),
       ],
     );
   }
 
-  Widget _ridebatton()
-  {
+  Widget _rideButton() {
     return Container(
-      margin: EdgeInsets.only(bottom: _deviceHeight *0.01),
-
+      margin: EdgeInsets.only(bottom: _deviceHeight * 0.01),
       width: _deviceWidth,
-     decoration: BoxDecoration(color: Colors.white,borderRadius:BorderRadius.circular(10) ),
-      child:MaterialButton(onPressed: () {},child: Text("book ride",style: TextStyle(color: Colors.blue),),),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: MaterialButton(
+        onPressed: () {},
+        child: const Text(
+          "Book Ride",
+          style: TextStyle(color: Colors.blue),
+        ),
+      ),
     );
   }
 }
